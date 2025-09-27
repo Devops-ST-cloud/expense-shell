4052a769053dfe578003df4411269192a0d3075c
Devops-ST-cloud <boyasuryateja77@gmail.com>
2025-09-27 17:26:59 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -10,6 +10,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+echo "please enter DB password"
+read mysql_root_password
+
 if [ $USERID -ne 0 ]
   then 
       echo "please run the script as root user"
@@ -48,20 +51,38 @@ else
 fi
 
 mkdir -p /app
-VALIDATE $? "creating app directory"
+VALIDATE $? "creating app directory" &>>$LOGFILE
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading backend code"
 
 cd /app
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>$LOGFILE
 VALIDATE $? "Extracted backend code"
 
-npm install
+npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependecies"
 
-#cp /home/ec2-user/expense-shell/backend.service /etc/ststemd/system/backend.service
+cp /home/ec2-user/expense-shell/backend.service /etc/ststemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "copied backend service"
 
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "demon reload"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "starting backend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing Mysql client"
+
+mysql -h db.DevOps-ST.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "schema loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "restaring backend"
 
 
 
